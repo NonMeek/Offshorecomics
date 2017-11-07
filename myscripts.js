d2e63be25d8dffcd7b3e563f7880e490028d76c5
Offshorecomics.com <stefan.gasic@gmail.com>
2017-11-07 21:31:08 +0200

    Tightened the HTML and moved the <script></script> part in the previous push to JS.

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,9 @@
-
+var imageArray = ["images/Strip1.jpg", "images/Strip2.PNG", "images/Strip3.PNG", "images/Strip4.PNG"];
+			var imageIndex = 1;
+		
+			function changeImage(){
+				var myImage = document.getElementById('mainImage');
+				myImage.setAttribute("src", imageArray[imageIndex]);
+				imageIndex++;
+				if (imageIndex > 3 ) {imageIndex = 0};
+			}
