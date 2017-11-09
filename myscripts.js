a4fded35e47435d60a403d28b808eb02528a306e
Offshorecomics.com <stefan.gasic@gmail.com>
2017-11-09 22:33:03 +0200

    Changed "changeImage" to "changePlus" ... just testing functionality

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,7 @@
 var imageArray = ["images/Strip1.jpg", "images/Strip2.PNG", "images/Strip3.PNG", "images/Strip4.PNG"];
 var imageIndex = 1;
 		
-	function changeImage(){
+	function changePlus(){
 		var myImage = document.getElementById('mainImage');
 		myImage.setAttribute("src", imageArray[imageIndex]);
 		imageIndex++;
