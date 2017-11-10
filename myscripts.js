a101a5eeeefafd09b28ba4b5e09c098ff37fe818
Offshorecomics.com <stefan.gasic@gmail.com>
2017-11-10 13:52:26 +0200

    Changed the absolute number 3 to "images.length -1" ...

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,9 @@
-var imageArray = ["images/Strip1.jpg", "images/Strip2.PNG", "images/Strip3.PNG", "images/Strip4.PNG"];
+var images = ["images/Strip1:jpg", "images/Strip2.PNG", "images/Strip3.PNG", "images/Strip4.PNG"];
 var imageIndex = 1;
-		
-	function changePlus(){
-		var myImage = document.getElementById('mainImage');
-		myImage.setAttribute("src", imageArray[imageIndex]);
-		imageIndex++;
-		if (imageIndex > 3 ) {imageIndex = 0};
-	}
+
+function changePlus(){
+	var myImage = document.getElementById("mainImage");
+	myImage.setAttribute("src", images[imageIndex]);
+	imageIndex++;
+	if (imageIndex > images.length - 1) {imageIndex = 1};
+}
