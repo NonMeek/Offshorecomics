e90dde00e5906e923ca6fb972ca306915c0b9550
Offshorecomics.com <stefan.gasic@gmail.com>
2017-11-11 09:55:51 +0200

    Adjusted yesterday's absolute array lenght number change to "images.length" ... now it works as a loop without forgetting the 1. image.

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,9 @@
-var images = ["images/Strip1:jpg", "images/Strip2.PNG", "images/Strip3.PNG", "images/Strip4.PNG"];
-var imageIndex = 1;
+var images = ["images/Strip1.jpg", "images/Strip2.PNG", "images/Strip3.PNG", "images/Strip4.PNG"];
+var imageIndex = 0;
 
 function changePlus(){
-	var myImage = document.getElementById("mainImage");
-	myImage.setAttribute("src", images[imageIndex]);
+	var myImage = document.getElementById('mainImage');
+	myImage.setAttribute('src', images[imageIndex]);
 	imageIndex++;
-	if (imageIndex > images.length - 1) {imageIndex = 1};
-}
+	if(imageIndex > images.length-1) {imageIndex = 0};
+	}
